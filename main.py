6c51b9b16dd336a844f2da99d9e5082bd43f494e
GrandDad7 <jose.tua@upr.edu>
2020-05-11 12:40:34 -0400

    Grabbing additional classes
    Making use of utils.py

--- main.py
+++ main.py
@@ -1,5 +1,63 @@
+import random
+import math
+import numpy as np
+from utils import *
+import sys
+from collections import deque
+
+
 #Python implementation of algorithms from Russell And Norvig's "Artificial Intelligence - A Modern Approach"
 
+class Problem:
+    """The abstract class for a formal problem. You should subclass
+    this and implement the methods actions and result, and possibly
+    __init__, goal_test, and path_cost. Then you will create instances
+    of your subclass and solve them with the various search functions."""
+
+    def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2. If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1
+
+    def value(self, state):
+        """For optimization problems, each state has a value. Hill Climbing
+        and related algorithms try to maximize this value."""
+        raise NotImplementedError
+
+########################################################################################################################
 class Node:
     """A node in a search tree. Contains a pointer to the parent (the node
     that this is a successor of) and to the actual state for this node. Note
@@ -63,3 +121,361 @@ class Node:
         # object itself to quickly search a node
         # with the same state in a Hash Table
         return hash(self.state)
+
+########################################################################################################################
+# Graphs and Graph Problems
+
+class Graph:
+    """A graph connects nodes (vertices) by edges (links). Each edge can also
+    have a length associated with it. The constructor call is something like:
+        g = Graph({'A': {'B': 1, 'C': 2})
+    this makes a graph with 3 nodes, A, B, and C, with an edge of length 1 from
+    A to B,  and an edge of length 2 from A to C. You can also do:
+        g = Graph({'A': {'B': 1, 'C': 2}, directed=False)
+    This makes an undirected graph, so inverse links are also added. The graph
+    stays undirected; if you add more links with g.connect('B', 'C', 3), then
+    inverse link is also added. You can use g.nodes() to get a list of nodes,
+    g.get('A') to get a dict of links out of A, and g.get('A', 'B') to get the
+    length of the link from A to B. 'Lengths' can actually be any object at
+    all, and nodes can be any hashable object."""
+
+    def __init__(self, graph_dict=None, directed=True):
+        self.graph_dict = graph_dict or {}
+        self.directed = directed
+        if not directed:
+            self.make_undirected()
+
+    def make_undirected(self):
+        """Make a digraph into an undirected graph by adding symmetric edges."""
+        for a in list(self.graph_dict.keys()):
+            for (b, dist) in self.graph_dict[a].items():
+                self.connect1(b, a, dist)
+
+    def connect(self, A, B, distance=1):
+        """Add a link from A and B of given distance, and also add the inverse
+        link if the graph is undirected."""
+        self.connect1(A, B, distance)
+        if not self.directed:
+            self.connect1(B, A, distance)
+
+    def connect1(self, A, B, distance):
+        """Add a link from A to B of given distance, in one direction only."""
+        self.graph_dict.setdefault(A, {})[B] = distance
+
+    def get(self, a, b=None):
+        """Return a link distance or a dict of {node: distance} entries.
+        .get(a,b) returns the distance or None;
+        .get(a) returns a dict of {node: distance} entries, possibly {}."""
+        links = self.graph_dict.setdefault(a, {})
+        if b is None:
+            return links
+        else:
+            return links.get(b)
+
+    def nodes(self):
+        """Return a list of nodes in the graph."""
+        s1 = set([k for k in self.graph_dict.keys()])
+        s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
+        nodes = s1.union(s2)
+        return list(nodes)
+
+
+def UndirectedGraph(graph_dict=None):
+    """Build a Graph where every edge (including future ones) goes both ways."""
+    return Graph(graph_dict=graph_dict, directed=False)
+
+
+def RandomGraph(nodes=list(range(10)), min_links=2, width=400, height=300,
+                curvature=lambda: random.uniform(1.1, 1.5)):
+    """Construct a random graph, with the specified nodes, and random links.
+    The nodes are laid out randomly on a (width x height) rectangle.
+    Then each node is connected to the min_links nearest neighbors.
+    Because inverse links are added, some nodes will have more connections.
+    The distance between nodes is the hypotenuse times curvature(),
+    where curvature() defaults to a random number between 1.1 and 1.5."""
+    g = UndirectedGraph()
+    g.locations = {}
+    # Build the cities
+    for node in nodes:
+        g.locations[node] = (random.randrange(width), random.randrange(height))
+    # Build roads from each city to at least min_links nearest neighbors.
+    for i in range(min_links):
+        for node in nodes:
+            if len(g.get(node)) < min_links:
+                here = g.locations[node]
+
+                def distance_to_node(n):
+                    if n is node or g.get(node, n):
+                        return np.inf
+                    return math.distance(g.locations[n], here)
+
+                neighbor = min(nodes, key=distance_to_node)
+                d = distance(g.locations[neighbor], here) * curvature()
+                g.connect(node, neighbor, int(d))
+    return g
+
+
+""" [Figure 3.2]
+Simplified road map of Romania
+"""
+romania_map = UndirectedGraph(dict(
+    Arad=dict(Zerind=75, Sibiu=140, Timisoara=118),
+    Bucharest=dict(Urziceni=85, Pitesti=101, Giurgiu=90, Fagaras=211),
+    Craiova=dict(Drobeta=120, Rimnicu=146, Pitesti=138),
+    Drobeta=dict(Mehadia=75),
+    Eforie=dict(Hirsova=86),
+    Fagaras=dict(Sibiu=99),
+    Hirsova=dict(Urziceni=98),
+    Iasi=dict(Vaslui=92, Neamt=87),
+    Lugoj=dict(Timisoara=111, Mehadia=70),
+    Oradea=dict(Zerind=71, Sibiu=151),
+    Pitesti=dict(Rimnicu=97),
+    Rimnicu=dict(Sibiu=80),
+    Urziceni=dict(Vaslui=142)))
+romania_map.locations = dict(
+    Arad=(91, 492), Bucharest=(400, 327), Craiova=(253, 288),
+    Drobeta=(165, 299), Eforie=(562, 293), Fagaras=(305, 449),
+    Giurgiu=(375, 270), Hirsova=(534, 350), Iasi=(473, 506),
+    Lugoj=(165, 379), Mehadia=(168, 339), Neamt=(406, 537),
+    Oradea=(131, 571), Pitesti=(320, 368), Rimnicu=(233, 410),
+    Sibiu=(207, 457), Timisoara=(94, 410), Urziceni=(456, 350),
+    Vaslui=(509, 444), Zerind=(108, 531))
+
+""" [Figure 4.9]
+Eight possible states of the vacumm world
+Each state is represented as
+   *       "State of the left room"      "State of the right room"   "Room in which the agent
+                                                                      is present"
+1 - DDL     Dirty                         Dirty                       Left
+2 - DDR     Dirty                         Dirty                       Right
+3 - DCL     Dirty                         Clean                       Left
+4 - DCR     Dirty                         Clean                       Right
+5 - CDL     Clean                         Dirty                       Left
+6 - CDR     Clean                         Dirty                       Right
+7 - CCL     Clean                         Clean                       Left
+8 - CCR     Clean                         Clean                       Right
+"""
+vacuum_world = Graph(dict(
+    State_1=dict(Suck=['State_7', 'State_5'], Right=['State_2']),
+    State_2=dict(Suck=['State_8', 'State_4'], Left=['State_2']),
+    State_3=dict(Suck=['State_7'], Right=['State_4']),
+    State_4=dict(Suck=['State_4', 'State_2'], Left=['State_3']),
+    State_5=dict(Suck=['State_5', 'State_1'], Right=['State_6']),
+    State_6=dict(Suck=['State_8'], Left=['State_5']),
+    State_7=dict(Suck=['State_7', 'State_3'], Right=['State_8']),
+    State_8=dict(Suck=['State_8', 'State_6'], Left=['State_7'])
+))
+
+""" [Figure 4.23]
+One-dimensional state space Graph
+"""
+one_dim_state_space = Graph(dict(
+    State_1=dict(Right='State_2'),
+    State_2=dict(Right='State_3', Left='State_1'),
+    State_3=dict(Right='State_4', Left='State_2'),
+    State_4=dict(Right='State_5', Left='State_3'),
+    State_5=dict(Right='State_6', Left='State_4'),
+    State_6=dict(Left='State_5')
+))
+one_dim_state_space.least_costs = dict(
+    State_1=8,
+    State_2=9,
+    State_3=2,
+    State_4=2,
+    State_5=4,
+    State_6=3)
+
+""" [Figure 6.1]
+Principal states and territories of Australia
+"""
+australia_map = UndirectedGraph(dict(
+    T=dict(),
+    SA=dict(WA=1, NT=1, Q=1, NSW=1, V=1),
+    NT=dict(WA=1, Q=1),
+    NSW=dict(Q=1, V=1)))
+australia_map.locations = dict(WA=(120, 24), NT=(135, 20), SA=(135, 30),
+                               Q=(145, 20), NSW=(145, 32), T=(145, 42),
+                               V=(145, 37))
+
+
+class GraphProblem(Problem):
+    """The problem of searching a graph from one node to another."""
+
+    def __init__(self, initial, goal, graph):
+        super().__init__(initial, goal)
+        self.graph = graph
+
+    def actions(self, A):
+        """The actions at a graph node are just its neighbors."""
+        return list(self.graph.get(A).keys())
+
+    def result(self, state, action):
+        """The result of going to a neighbor is just that neighbor."""
+        return action
+
+    def path_cost(self, cost_so_far, A, action, B):
+        return cost_so_far + (self.graph.get(A, B) or np.inf)
+
+    def find_min_edge(self):
+        """Find minimum value of edges."""
+        m = np.inf
+        for d in self.graph.graph_dict.values():
+            local_min = min(d.values())
+            m = min(m, local_min)
+
+        return m
+
+    def h(self, node):
+        """h function is straight-line distance from a node's state to goal."""
+        locs = getattr(self.graph, 'locations', None)
+        if locs:
+            if type(node) is str:
+                return int(distance(locs[node], locs[self.goal]))
+
+            return int(distance(locs[node.state], locs[self.goal]))
+        else:
+            return np.inf
+
+
+class GraphProblemStochastic(GraphProblem):
+    """
+    A version of GraphProblem where an action can lead to
+    nondeterministic output i.e. multiple possible states.
+    Define the graph as dict(A = dict(Action = [[<Result 1>, <Result 2>, ...], <cost>], ...), ...)
+    A the dictionary format is different, make sure the graph is created as a directed graph.
+    """
+
+    def result(self, state, action):
+        return self.graph.get(state, action)
+
+    def path_cost(self):
+        raise NotImplementedError
+
+
+########################################################################################################################
+
+# Code to compare searchers on various problems.
+
+
+class InstrumentedProblem(Problem):
+    """Delegates to a problem, and keeps statistics."""
+
+    def __init__(self, problem):
+        self.problem = problem
+        self.succs = self.goal_tests = self.states = 0
+        self.found = None
+
+    def actions(self, state):
+        self.succs += 1
+        return self.problem.actions(state)
+
+    def result(self, state, action):
+        self.states += 1
+        return self.problem.result(state, action)
+
+    def goal_test(self, state):
+        self.goal_tests += 1
+        result = self.problem.goal_test(state)
+        if result:
+            self.found = state
+        return result
+
+    def path_cost(self, c, state1, action, state2):
+        return self.problem.path_cost(c, state1, action, state2)
+
+    def value(self, state):
+        return self.problem.value(state)
+
+    def __getattr__(self, attr):
+        return getattr(self.problem, attr)
+
+    def __repr__(self):
+        return '<{:4d}/{:4d}/{:4d}/{}>'.format(self.succs, self.goal_tests,
+                                               self.states, str(self.found)[:4])
+
+########################################################################################################################
+
+def exp_schedule(k=20, lam=0.005, limit=100):
+    """One possible schedule function for simulated annealing"""
+    return lambda t: (k * np.exp(-lam * t) if t < limit else 0)
+
+def astar_search(problem, h=None, display=False):
+    """A* search is best-first graph search with f(n) = g(n)+h(n).
+    You need to specify the h function when you call astar_search, or
+    else in your Problem subclass."""
+    h = memoize(h or problem.h, 'h')
+    return best_first_graph_search(problem, lambda n: n.path_cost + h(n), display)
+
+def recursive_best_first_search(problem, h=None):
+    """[Figure 3.26]"""
+    h = memoize(h or problem.h, 'h')
+
+    def RBFS(problem, node, flimit):
+        if problem.goal_test(node.state):
+            return node, 0  # (The second value is immaterial)
+        successors = node.expand(problem)
+        if len(successors) == 0:
+            return None, np.inf
+        for s in successors:
+            s.f = max(s.path_cost + h(s), node.f)
+        while True:
+            # Order by lowest f value
+            successors.sort(key=lambda x: x.f)
+            best = successors[0]
+            if best.f > flimit:
+                return None, best.f
+            if len(successors) > 1:
+                alternative = successors[1].f
+            else:
+                alternative = np.inf
+            result, best.f = RBFS(problem, best, min(flimit, alternative))
+            if result is not None:
+                return result, best.f
+
+    node = Node(problem.initial)
+    node.f = h(node)
+    result, bestf = RBFS(problem, node, np.inf)
+    return result
+
+
+def simulated_annealing(problem, schedule=exp_schedule()):
+    """[Figure 4.5] CAUTION: This differs from the pseudocode as it
+    returns a state instead of a Node."""
+    current = Node(problem.initial)
+    for t in range(sys.maxsize):
+        T = schedule(t)
+        if T == 0:
+            return current.state
+        neighbors = current.expand(problem)
+        if not neighbors:
+            return current.state
+        next_choice = random.choice(neighbors)
+        delta_e = problem.value(next_choice.state) - problem.value(current.state)
+        if delta_e > 0 or probability(np.exp(delta_e / T)):
+            current = next_choice
+
+########################################################################################################################
+
+def compare_searchers(problems, header,
+                      searchers=[breadth_first_tree_search,
+                                 breadth_first_graph_search,
+                                 depth_first_graph_search,
+                                 iterative_deepening_search,
+                                 depth_limited_search,
+                                 recursive_best_first_search]):
+    def do(searcher, problem):
+        p = InstrumentedProblem(problem)
+        searcher(p)
+        return p
+
+    table = [[name(s)] + [do(s, p) for p in problems] for s in searchers]
+    print_table(table, header)
+
+
+def compare_graph_searchers():
+    """Prints a table of search results."""
+    compare_searchers(problems=[GraphProblem('Arad', 'Bucharest', romania_map),
+                                GraphProblem('Oradea', 'Neamt', romania_map),
+                                GraphProblem('Q', 'WA', australia_map)],
+                      header=['Searcher', 'romania_map(Arad, Bucharest)',
+                              'romania_map(Oradea, Neamt)', 'australia_map'])
